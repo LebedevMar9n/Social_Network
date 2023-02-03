e7c5bcf0ebc2a225f78e38505452b1f560ac5c90
LebedevMar9n <lebedevmar9n1991@gmail.com>
2023-02-03 15:47:51 +0200

    Implemented BrowserRouter on client side (Added home,auth routes)

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './App';
 import store from './store/ReduxStore';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />,
+    <BrowserRouter>
+      <Routes>
+        <Route path='*' element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
